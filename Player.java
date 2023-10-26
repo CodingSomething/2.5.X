49a8e390c993904eae5006a83d636e790493cf25
CodingSomething <79930693+CodingSomething@users.noreply.github.com>
2023-10-26 13:57:56 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -8,10 +8,26 @@ import java.util.Scanner;
 public class Player
 {
   /* your code here - attributes */
-
+  private int points = 0;
+  private boolean isTurn;
+  private String name;
   /* your code here - constructor(s) */ 
-
+  public Player(String name){
+    this.name = name;
+  }
   /* your code here - accessor(s) */ 
-
+  public int getPoints(){
+    return this.points;
+  }
+  public boolean getTurn(){
+    return this.isTurn;
+  }
   /* your code here - mutator(s) */ 
+  public void setPoints(int value){
+    this.points = value;
+  }
+  public void setTurn(Boolean bool){
+    this.isTurn = bool;
+  }
+  
 }
