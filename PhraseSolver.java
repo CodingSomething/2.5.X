5b6c7100e0b35ef8f32248ac68c7de0c7a92aea8
CodingSomething <79930693+CodingSomething@users.noreply.github.com>
2023-10-26 13:58:25 -0700

    Update PhraseSolver.java

--- PhraseSolver.java
+++ PhraseSolver.java
@@ -8,9 +8,19 @@ import java.util.Scanner;
 public class PhraseSolver
 {
   /* your code here - attributes */
-
+  private boolean solved;
+  private Board board;
+  private Player player1;
+  private Player player2;
   /* your code here - constructor(s) */ 
+  public PhraseSolver(){
 
+  }
+  System.out.println("Please enter the name of the first player.");
+  player1 = new Player(sc.NextLine());    
+  System.out.println("Please enter the name of the second player.");
+  player2 = new Player(sc.NextLine());
+  board = new Board();
   /* your code here - accessor(s) */
   
   /* your code here - mutator(s)  */
@@ -23,9 +33,12 @@ public class PhraseSolver
     Scanner input = new Scanner(System.in);
     
     boolean correct = true;
+   
+
     while (!solved) 
     {
-      
+
+      }
       /* your code here - game logic */
       
       
